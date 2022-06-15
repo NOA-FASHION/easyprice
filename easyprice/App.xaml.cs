a957e2a4eaba865639e83a147ebbae4b72ed0e86
Almont michel <michel.almont@gmail.com>
2022-06-14 21:06:50 -0400

    new interface 3

--- easyprice/App.xaml.cs
+++ easyprice/App.xaml.cs
@@ -11,30 +11,30 @@ namespace easyprice
             InitializeComponent();
 
 
-            if (!Application.Current.Properties.ContainsKey("activation"))
-            {
-                activaApp();
+            //if (!Application.Current.Properties.ContainsKey("activation"))
+            //{
+            //    activaApp();
 
-            }
-            if (Application.Current.Properties.ContainsKey("activation"))
-            {
-                var result = Application.Current.Properties["activation"].ToString();
+            //}
+            //if (Application.Current.Properties.ContainsKey("activation"))
+            //{
+            //    var result = Application.Current.Properties["activation"].ToString();
 
-                if (result == "true")
-                {
-                    MainPage = new NavigationPage(new MyTabbedPage());
-                }
-                else if (result == "false")
-                {
-                    MainPage = new NavigationPage(new InAppPurchase());
-                }
-            }
-            else
-            {
-                MainPage = new NavigationPage(new MyTabbedPage());
-            }
+            //    if (result == "true")
+            //    {
+            //        MainPage = new NavigationPage(new MyTabbedPage());
+            //    }
+            //    else if (result == "false")
+            //    {
+            //        MainPage = new NavigationPage(new InAppPurchase());
+            //    }
+            //}
+            //else
+            //{
+            //    MainPage = new NavigationPage(new MyTabbedPage());
+            //}
 
-            //MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new MyTabbedPage());
 
         }
         void activaApp()
